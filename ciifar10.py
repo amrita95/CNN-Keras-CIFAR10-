f3aa97a7d9923395aceb01c9b6230920d501fd68
Amrita Sundari Veerabaghu <amrita95@tamu.edu>
2018-02-24 17:26:00 -0600

    imported dataset cifar10

--- ciifar10.py
+++ ciifar10.py
@@ -3,3 +3,5 @@ from keras.layers import Conv2D,Dense,Dropout,MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
 
 from keras.models import Sequential
+
+from keras.datasets import cifar10
