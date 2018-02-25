06dc3037826c5d2db29d62af0014bf688440d3a7
Amrita Sundari Veerabaghu <amrita95@tamu.edu>
2018-02-25 02:29:48 -0600

    model completed

--- ciifar10.py
+++ ciifar10.py
@@ -1,9 +1,70 @@
 import keras
-from keras.layers import Conv2D,Dense,Dropout,MaxPooling2D
+from keras.layers import Conv2D,Dense,Dropout,MaxPooling2D,Activation,Flatten
 from keras.preprocessing.image import ImageDataGenerator
-
 from keras.models import Sequential
-
 from keras.datasets import cifar10
-
+import numpy as np
+import pandas as pd
 import os
+import matplotlib.pyplot as plt
+
+
+(train_features, train_labels), (test_features, test_labels) = cifar10.load_data()
+classes = np.unique(train_labels)
+num_classes= len(classes)
+
+class_names = ['airplane','automobile','bird','cat','deer',
+               'dog','frog','horse','ship','truck']
+fig = plt.figure(figsize=(8,3))
+for i in range(num_classes):
+    ax = fig.add_subplot(2, 5, 1 + i, xticks=[], yticks=[])
+    idx = np.where(train_labels[:]==i)[0]
+    features_idx = train_features[idx,::]
+    img_num = np.random.randint(features_idx.shape[0])
+    #im = np.transpose(features_idx[img_num,::], (1, 2, 0))
+    ax.set_title(class_names[i])
+    plt.imshow(features_idx[img_num,::])
+plt.show()
+
+train_labels = keras.utils.to_categorical(train_labels,num_classes)
+test_labels = keras.utils.to_categorical(test_labels,num_classes)
+
+model = Sequential()
+model.add(Conv2D(32,(3,3),padding = 'same',input_shape=(32,32,3)))
+model.add(Activation('relu'))
+model.add(Conv2D(32,(3,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=opt,
+              metrics=['accuracy'])
+
+print(model.summary())
+
+train_features = train_features.astype('float32')/255
+test_features = test_features.astype('float32')/255
+
+model.fit(train_features,train_labels,
+              batch_size=128,
+              epochs=5,
+              validation_data=(test_features, test_labels),
+              shuffle=True)
+
